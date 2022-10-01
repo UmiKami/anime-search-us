a931c72aa74e9ff28b3f5260738fde5b7e95033b
umikami <ggoku02@gmail.com>
2022-09-30 22:37:13 -0400

    Finished basic responsive version of details page

--- src/View/AnimeDetailsPage.jsx
+++ src/View/AnimeDetailsPage.jsx
@@ -1,116 +1,186 @@
 import axios from "axios";
-import "../styles/AnimeDetails.css"
-import { useState } from "react";
-import { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import Maintenance from "../Components/Maintenance";
+import "../styles/AnimeDetails.css";
+import { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import useWindowDimensions from "../Hooks/WindowDimensions";
+import Navbar from "../Components/Navbar";
 
 const AnimeDetailsPage = () => {
+    // gets the param from the URL if any
     const { animeId } = useParams();
-    const {width} = useWindowDimensions();
+    // allows us to redirect
+    const navigate = useNavigate();
+    // stores incoming data about specific anime with animeId
     const [animeData, setAnimeData] = useState([]);
 
-    console.log(width);
-
-    // JSON.parse() turns string true or false into boolean 
+    // JSON.parse() turns string true or false into boolean
     const underMaintenance = JSON.parse(process.env.REACT_APP_MAINTENANCE);
+
+    // formats date string given by the API in| MONTH DD, YYYY | format
+    const formatDate = (date) => {
+        let dateArr = date.split("-")
+
+        // hasmap for incoming numbered month date format
+        let monthsMap = {
+            "01": "January",
+            "02": "February",
+            "03": "March",
+            "04": "April",
+            "05": "May",
+            "06": "June",
+            "07": "July",
+            "08": "August",
+            "09": "September",
+            "10": "October",
+            "11": "Novemeber",
+            "12": "December"
+        }
+
+        return `${monthsMap[dateArr[1]]} ${dateArr[2]}, ${dateArr[0]}`
+    }
 
     useEffect(() => {
         // change when updating the page or when it is broken
+        if (underMaintenance) {
+            navigate("/maintenance");
+        }
+
         axios
             .get(`https://kitsu.io/api/edge/anime/${animeId}`)
             .then((res) => {
                 setAnimeData(res.data.data);
             })
-            .catch((error) => console.log(error));
+            .catch((error) => console.log("Error:", error));
     }, [animeId]);
-
-    // console.log(animeData);
 
     return (
         <main className="mainContainer">
-            {!underMaintenance ? (
-                <>
-                    {" "}
-                    <Link
-                        to={"/"}
-                        style={{ textDecoration: "none", cursor: "pointer" }}
-                        className="text-center mt-5"
-                    >
-                        <h1 className="mb-5 text-success">Anime Search</h1>
-                    </Link>
-                    <div className="container-fluid d-flex custom-container">
-                        {animeData.length !== 0 ? (
-                            <div className="row row-custom justify-content-center">
-                                <div className={"summary col-lg-" + (width > 1700 && width <= 1920? "2" : width <= 1200 ? "5" : width > 1921 ? "3" : "3")}>
-                                    <img
-                                        src={
-                                            animeData.attributes.posterImage
-                                                .medium
-                                        }
-                                        alt="anime poster"
-                                        className="poster col-12"
-                                    />
-                                    <div className="summary-details col-12">
-                                        <p>
-                                            Rating:{" "}
-                                            <span className="badge badge-success"></span>
-                                        </p>
-                                        <p>
-                                            Type:{" "}
-                                            <span className="summary-details-vals"></span>
-                                        </p>
-                                        <p>
-                                            Status:{" "}
-                                            <span className="badge badge-danger"></span>
-                                        </p>
-                                        <p>
-                                            Start Date:{" "}
-                                            <span className="summary-details-vals"></span>
-                                        </p>
-                                        <p>
-                                            End Date:{" "}
-                                            <span className="summary-details-vals"></span>
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <div className="anime-description ms-5 col-lg-7">
-                                    <h1 className="anime-description-header mb-4">
-                                        {Object.values(
-                                            animeData.attributes.titles
-                                        )[0]
-                                            ? Object.values(
-                                                  animeData.attributes.titles
-                                              )[0]
-                                            : Object.values(
-                                                  animeData.attributes.titles
-                                              )[1]}
-                                    </h1>
-                                    <p className="fs-5 anime-description-p">
-                                        {animeData.attributes.synopsis
-                                            ? animeData.attributes.synopsis
-                                            : animeData.attributes.description
-                                            ? animeData.attributes.description
-                                            : "No description available,"}
-                                    </p>
-                                </div>
+            <div className="container-lg py-3 px-0">
+                <Navbar />
+            </div>
+            {animeData.attributes ? (
+                <div className="container-lg">
+                    <div className="row gx-lg-5">
+                        <div
+                            className="card col-2 px-0 summary-details"
+                            style={{ width: "24rem", height: "min-content" }}
+                        >
+                            <img
+                                src={
+                                    animeData.attributes.posterImage
+                                        ? animeData.attributes.posterImage
+                                              .medium
+                                        : animeData.attributes.posterImage
+                                              .original
+                                }
+                                className="card-img-top"
+                                alt={
+                                    Object.values(
+                                        animeData.attributes.titles
+                                    )[0]
+                                        ? Object.values(
+                                              animeData.attributes.titles
+                                          )[0] + " poster"
+                                        : Object.values(
+                                              animeData.attributes.titles
+                                          )[1] + " poster"
+                                }
+                            />
+                            <div className="card-body">
+                                <h5 className="py-2 text-info-1">
+                                    Rating:{" "}
+                                    <span
+                                        className={`badge rounded-pill fs-6 text-bg-${
+                                            animeData.attributes.averageRating
+                                                ? parseFloat(
+                                                      animeData.attributes
+                                                          .averageRating
+                                                  ) >= 79
+                                                    ? "success light-success"
+                                                    : parseFloat(
+                                                          animeData.attributes
+                                                              .averageRating
+                                                      ) >= 59
+                                                    ? "warning light-warning"
+                                                    : "danger light-danger"
+                                                : "secondary"
+                                        }`}
+                                    >
+                                        {animeData.attributes.averageRating
+                                            ? animeData.attributes.averageRating
+                                            : "Not available"}
+                                    </span>
+                                </h5>
+                                <h5 className="py-2 text-info-1">
+                                    Type:{" "}
+                                    <span className="anime-attr text-light">
+                                        {animeData.attributes.subtype
+                                            ? animeData.attributes.subtype
+                                            : "Not available"}
+                                    </span>
+                                </h5>
+                                <h5 className="py-2 text-info-1">
+                                    Status:{" "}
+                                    {animeData.attributes.status ===
+                                    "finished" ? (
+                                        <span className="badge bg-danger ms-2 fs-6">
+                                            Finished
+                                        </span>
+                                    ) : animeData.attributes.status ===
+                                      "current" ? (
+                                        <span className="badge bg-success ms-2 fs-6">
+                                            Airing
+                                        </span>
+                                    ) : (
+                                        <span className="badge bg-warning ms-2 fs-6">
+                                            Coming Soon
+                                        </span>
+                                    )}
+                                </h5>
+                                <h5 className="py-2 text-info-1">
+                                    Start Date:&nbsp;
+                                    <span className="anime-attr text-light">
+                                        {animeData.attributes.startDate
+                                            ? formatDate(
+                                                  animeData.attributes.startDate
+                                              )
+                                            : "Not available"}
+                                    </span>
+                                </h5>
+                                <h5 className="py-2 text-info-1">
+                                    End Date:&nbsp;
+                                    <span className="anime-attr text-light">
+                                        {animeData.attributes.endDate
+                                            ? formatDate(
+                                                  animeData.attributes.endDate
+                                              )
+                                            : "Not available"}
+                                    </span>
+                                </h5>
                             </div>
-                        ) : (
-                            <div
-                                className="spinner-border spinner-border-lg"
-                                role="status"
-                            >
-                                <span className="visually-hidden">
-                                    Loading...
-                                </span>
-                            </div>
-                        )}
+                        </div>
+                        <div className="col">
+                            <h1 className="mb-5 anime-detail-title">
+                                {Object.values(animeData.attributes.titles)[0]
+                                    ? Object.values(
+                                          animeData.attributes.titles
+                                      )[0]
+                                    : Object.values(
+                                          animeData.attributes.titles
+                                      )[1]}
+                            </h1>
+                            <p className="text-light fs-5 lh-lg">
+                                {animeData.attributes.synopsis ||
+                                animeData.attributes.description
+                                    ? animeData.attributes.synopsis ||
+                                      animeData.attributes.description
+                                    : "(No description available!)"}
+                            </p>
+                        </div>
                     </div>
-                </>
+                </div>
             ) : (
-                <Maintenance />
+                <span className="visually-hidden">Loading...</span>
             )}
         </main>
     );
