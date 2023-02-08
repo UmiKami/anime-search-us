317bd97b96350faff66d93bc57691b7f8ba7a0b7
UmiKami <ggoku02@gmail.com>
2023-02-07 22:03:54 -0500

    migrated pagination toolbar to its own react component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useNavigate, useParams, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import FilterBar from "./Components/FilterBar";
 import NothingFound from "./Components/NothingFound";
+import PaginationBar from "./Components/PaginationBar";
 
 function App() {
     // reset title value to original state when at home page
@@ -79,72 +80,7 @@ function App() {
                           })
                         : <NothingFound/>}
                 </div>
-                {animeList.length ? <nav
-                    aria-label="Page navigation example"
-                    className="d-flex justify-content-center"
-                >
-                    <ul className="pagination">
-                        <li
-                            className={
-                                "page-item " +
-                                (parseInt(pageId) === 1 && "disabled")
-                            }
-                        >
-                            <Link
-                                className="page-link"
-                                to={
-                                    "/page/" +
-                                    (pageId > 1 ? parseInt(pageId) - 1 : 1)
-                                }
-                                aria-label="Previous"
-                            >
-                                <span aria-hidden="true">&laquo;</span>
-                            </Link>
-                        </li>
-                        <li className="page-item">
-                            <Link
-                                className="page-link active"
-                                to={"/page/" + (pageId ? pageId : 1)}
-                            >
-                                {pageId ? pageId : 1}
-                            </Link>
-                        </li>
-                        <li className="page-item">
-                            <Link
-                                className="page-link"
-                                to={
-                                    "/page/" +
-                                    (pageId ? parseInt(pageId) + 1 : 2)
-                                }
-                            >
-                                {pageId ? parseInt(pageId) + 1 : 2}
-                            </Link>
-                        </li>
-                        <li className="page-item">
-                            <Link
-                                className="page-link"
-                                to={
-                                    "/page/" +
-                                    (pageId ? parseInt(pageId) + 2 : 3)
-                                }
-                            >
-                                {pageId ? parseInt(pageId) + 2 : 3}
-                            </Link>
-                        </li>
-                        <li className="page-item">
-                            <Link
-                                className="page-link"
-                                to={
-                                    "/page/" +
-                                    (pageId ? parseInt(pageId) + 1 : 2)
-                                }
-                                aria-label="Next"
-                            >
-                                <span aria-hidden="true">&raquo;</span>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav> : <br/>}
+                <PaginationBar animeList={animeList} pageId={pageId}/>
             </div>
         </div>
     );
