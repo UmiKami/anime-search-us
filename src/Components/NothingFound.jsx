abadde546672b5825253b6b6309856e2b404d5ed
UmiKami <ggoku02@gmail.com>
2023-02-07 22:22:36 -0500

    meme is now responsive on all devices

--- src/Components/NothingFound.jsx
+++ src/Components/NothingFound.jsx
@@ -14,7 +14,7 @@ const NothingFound = () => {
       <div>
           <h1>No anime found 😔</h1>
           <h4>But hey!! Look at this awesome meme!</h4>
-          {animeMeme && <img src={animeMeme.preview[animeMeme.preview.length - 1]} alt={animeMeme.title} />}
+          {animeMeme && <img src={animeMeme.preview[animeMeme.preview.length - 1]} alt={animeMeme.title} className="img-fluid col-sm-7 col-md-6 col-12" />}
       </div>
   );
 }
